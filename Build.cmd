86eaaf93c8ee53d8e5e10acb264315fbbc4f21ff
AndrewPeters <anpete@microsoft.com>
2012-03-20 15:10:43 -0700

    Tweak build

--- Build.cmd
+++ Build.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild EF.msbuild /v:minimal /maxcpucount %*
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\msbuild EF.msbuild /v:minimal /maxcpucount /nodeReuse:false %*
