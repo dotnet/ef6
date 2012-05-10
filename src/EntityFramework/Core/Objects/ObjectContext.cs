642b6fd26b07b323789c51680a3a83825f2c916d
AndriySvyryd <Andriy.Svyryd@microsoft.com>
2012-05-09 23:41:23 -0700

    Fixing NRE caused by the previous commit.

--- src/EntityFramework/Core/Objects/ObjectContext.cs
+++ src/EntityFramework/Core/Objects/ObjectContext.cs
@@ -980,7 +980,7 @@ namespace System.Data.Entity.Core.Objects
         /// <returns>An enumeration of objects of type <typeparamref name="TElement"/>.</returns>
         public ObjectResult<TElement> ExecuteStoreQuery<TElement>(string commandText, params object[] parameters)
         {
-            return ExecuteStoreQuery<TElement>(commandText, /*entitySetName:*/null, MergeOption.AppendOnly, parameters);
+            return _internalObjectContext.ExecuteStoreQuery<TElement>(commandText, /*entitySetName:*/null, MergeOption.AppendOnly, parameters);
         }
 
         /// <summary>
