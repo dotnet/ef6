694e5a47fc7f177edc25dc2248b097d0036c47d4
AndriySvyryd <Andriy.Svyryd@microsoft.com>
2012-05-29 10:35:36 -0700

    Reverting ObjectContext wrapping. Making all public members virtual. Refactoring SaveChanges.

---
+++ test/EntityFramework/UnitTests/Core/Objects/TestHelpers/DbProviderFactoryForMock.cs
@@ -0,0 +1,25 @@
+﻿namespace System.Data.Entity.Core.Objects
+{
+    using System.Data.Common;
+    using System.Data.Entity.Core.EntityClient.Internal;
+    using Moq;
+
+    public class DbProviderFactoryForMock : DbProviderFactory, IServiceProvider
+    {
+        private Mock<IEntityAdapter> entityAdapterMock;
+        public DbProviderFactoryForMock()
+        {
+            entityAdapterMock = new Mock<IEntityAdapter>();
+        }
+
+        public virtual object GetService(Type serviceType)
+        {
+            if (serviceType == typeof(IEntityAdapter))
+            {
+                return entityAdapterMock.Object;
+            }
+
+            return null;
+        }
+    }
+}
