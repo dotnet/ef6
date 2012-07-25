ba9a5430e274dcc008a3381c13b367a21f32b5d0
AndrewPeters <anpete@microsoft.com>
2012-07-25 09:51:48 -0700

    Fixing some file headers

--- src/PowerTools/GlobalSuppressions.cs
+++ src/PowerTools/GlobalSuppressions.cs
@@ -1,2 +1,2 @@
-﻿// ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1017:MarkAssembliesWithComVisible")]
