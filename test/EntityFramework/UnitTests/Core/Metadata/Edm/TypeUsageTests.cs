45932bcd3c207747f77f3630d3f6f8ac07fe2b21
maumar <maumar@microsoft.com>
2012-11-11 22:10:44 -0800

    Adding Metadata unit tests. CR: PawelKa

--- test/EntityFramework/UnitTests/Core/Metadata/Edm/TypeUsageTests.cs
+++ test/EntityFramework/UnitTests/Core/Metadata/Edm/TypeUsageTests.cs
@@ -2,6 +2,8 @@
 
 namespace System.Data.Entity.Core.Metadata.Edm
 {
+    using Moq;
+    using System.Data.Entity.Resources;
     using Xunit;
 
     public class TypeUsageTests
@@ -34,6 +36,60 @@ namespace System.Data.Entity.Core.Metadata.Edm
             Assert.Equal(5, typeUsage1.Facets.Count);
             Assert.Equal(6, typeUsage2.Facets.Count);
             Assert.Equal(ConcurrencyMode.Fixed, typeUsage2.Facets["ConcurrencyMode"].Value);
+        }
+
+        public class CreateStringTypeUsage
+        {
+            [Fact]
+            public void Exception_thrown_when_primitive_type_is_not_string()
+            {
+                var primitiveTypeMock = new Mock<PrimitiveType>();
+                primitiveTypeMock.SetupGet(m => m.PrimitiveTypeKind).Returns(PrimitiveTypeKind.Binary);
+
+                Assert.Equal(
+                    Strings.NotStringTypeForTypeUsage,
+                    Assert.Throws<ArgumentException>(() => TypeUsage.CreateStringTypeUsage(primitiveTypeMock.Object, isUnicode: true, isFixedLength: false)).Message);
+            }
+
+            [Fact]
+            public void Exception_thrown_when_size_is_less_than_one()
+            {
+                var primitiveTypeMock = new Mock<PrimitiveType>();
+                primitiveTypeMock.SetupGet(m => m.PrimitiveTypeKind).Returns(PrimitiveTypeKind.String);
+
+                Assert.True(
+                    Assert.Throws<ArgumentOutOfRangeException>(() => TypeUsage.CreateStringTypeUsage(primitiveTypeMock.Object, isUnicode: true, isFixedLength: false, maxLength: 0)).Message.StartsWith(Strings.InvalidMaxLengthSize));
+
+                Assert.True(
+                    Assert.Throws<ArgumentOutOfRangeException>(() => TypeUsage.CreateStringTypeUsage(primitiveTypeMock.Object, isUnicode: true, isFixedLength: false, maxLength: -10)).Message.StartsWith(Strings.InvalidMaxLengthSize));
+            }
+        }
+
+        public class CreateBinaryTypeUsage
+        {
+            [Fact]
+            public void Exception_thrown_when_primitive_type_is_not_binary()
+            {
+                var primitiveTypeMock = new Mock<PrimitiveType>();
+                primitiveTypeMock.SetupGet(m => m.PrimitiveTypeKind).Returns(PrimitiveTypeKind.String);
+
+                Assert.Equal(
+                    Strings.NotBinaryTypeForTypeUsage,
+                    Assert.Throws<ArgumentException>(() => TypeUsage.CreateBinaryTypeUsage(primitiveTypeMock.Object, isFixedLength: false)).Message);
+            }
+
+            [Fact]
+            public void Exception_thrown_when_size_is_less_than_one()
+            {
+                var primitiveTypeMock = new Mock<PrimitiveType>();
+                primitiveTypeMock.SetupGet(m => m.PrimitiveTypeKind).Returns(PrimitiveTypeKind.Binary);
+
+                Assert.True(
+                    Assert.Throws<ArgumentOutOfRangeException>(() => TypeUsage.CreateBinaryTypeUsage(primitiveTypeMock.Object, isFixedLength: false, maxLength: 0)).Message.StartsWith(Strings.InvalidMaxLengthSize));
+
+                Assert.True(
+                    Assert.Throws<ArgumentOutOfRangeException>(() => TypeUsage.CreateBinaryTypeUsage(primitiveTypeMock.Object, isFixedLength: false, maxLength: -10)).Message.StartsWith(Strings.InvalidMaxLengthSize));
+            }
         }
     }
 }
