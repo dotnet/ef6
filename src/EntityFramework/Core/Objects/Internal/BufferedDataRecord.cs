efa6ead91eb7d9e734d7663f19b5b955b239b673
AndriySvyryd <Andriy.Svyryd@microsoft.com>
2013-01-30 12:30:50 -0800

    Improving BufferedDataRecord performance by 1. Removing the base class thus eliminating virtual calls and allowing some methods to be inlined 2. Changing the comparison in IsDBNull

--- src/EntityFramework/Core/Objects/Internal/BufferedDataRecord.cs
+++ src/EntityFramework/Core/Objects/Internal/BufferedDataRecord.cs
@@ -4,7 +4,6 @@ namespace System.Data.Entity.Core.Objects.Internal
 {
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
-    using System.Data.Common;
     using System.Data.Entity.Utilities;
     using System.Diagnostics;
 #if !NET40
@@ -13,7 +12,7 @@ namespace System.Data.Entity.Core.Objects.Internal
     using System.Threading.Tasks;
 #endif
 
-    internal class BufferedDataRecord : DbDataRecord
+    internal class BufferedDataRecord
     {
         private int _currentRowNumber = -1;
         private object[] _currentRow;
@@ -42,12 +41,12 @@ namespace System.Data.Entity.Core.Objects.Internal
             _fieldNameLookup = new FieldNameLookup(new ReadOnlyCollection<string>(columnNames), -1);
         }
 
-        public override object this[string name]
+        public object this[string name]
         {
             get { return GetValue(GetOrdinal(name)); }
         }
 
-        public override object this[int ordinal]
+        public object this[int ordinal]
         {
             get { return GetValue(ordinal); }
         }
@@ -56,83 +55,70 @@ namespace System.Data.Entity.Core.Objects.Internal
 
         public bool HasRows
         {
-            get
-            {
-                return _resultSetCount > 0;
-            }
+            get { return _resultSetCount > 0; }
         }
 
-        public override int FieldCount
+        public int FieldCount
         {
             get { return _dataTypeNames.Length; }
         }
 
-        public override bool GetBoolean(int ordinal)
+        public bool GetBoolean(int ordinal)
         {
             return GetFieldValue<bool>(ordinal);
         }
 
-        public override byte GetByte(int ordinal)
+        public byte GetByte(int ordinal)
         {
             return GetFieldValue<byte>(ordinal);
         }
-
-        public override long GetBytes(int ordinal, long dataOffset, byte[] buffer, int bufferOffset, int length)
-        {
-            throw new NotSupportedException();
-        }
-
-        public override char GetChar(int ordinal)
+        
+        public char GetChar(int ordinal)
         {
             return GetFieldValue<char>(ordinal);
         }
-
-        public override long GetChars(int ordinal, long dataOffset, char[] buffer, int bufferOffset, int length)
-        {
-            throw new NotSupportedException();
-        }
-
-        public override DateTime GetDateTime(int ordinal)
+        
+        public DateTime GetDateTime(int ordinal)
         {
             return GetFieldValue<DateTime>(ordinal);
         }
 
-        public override decimal GetDecimal(int ordinal)
+        public decimal GetDecimal(int ordinal)
         {
             return GetFieldValue<decimal>(ordinal);
         }
 
-        public override double GetDouble(int ordinal)
+        public double GetDouble(int ordinal)
         {
             return GetFieldValue<double>(ordinal);
         }
 
-        public override float GetFloat(int ordinal)
+        public float GetFloat(int ordinal)
         {
             return GetFieldValue<float>(ordinal);
         }
 
-        public override Guid GetGuid(int ordinal)
+        public Guid GetGuid(int ordinal)
         {
             return GetFieldValue<Guid>(ordinal);
         }
 
-        public override short GetInt16(int ordinal)
+        public short GetInt16(int ordinal)
         {
             return GetFieldValue<short>(ordinal);
         }
 
-        public override int GetInt32(int ordinal)
+        public int GetInt32(int ordinal)
         {
             return GetFieldValue<int>(ordinal);
         }
 
-        public override long GetInt64(int ordinal)
+        public long GetInt64(int ordinal)
         {
             return GetFieldValue<long>(ordinal);
         }
 
-        public override string GetString(int ordinal)
+        public string GetString(int ordinal)
         {
             return GetFieldValue<string>(ordinal);
         }
@@ -152,12 +138,12 @@ namespace System.Data.Entity.Core.Objects.Internal
 
 #endif
 
-        public override object GetValue(int ordinal)
+        public object GetValue(int ordinal)
         {
             return GetFieldValue<object>(ordinal);
         }
 
-        public override int GetValues(object[] values)
+        public int GetValues(object[] values)
         {
             var count = Math.Min(values.Length, FieldCount);
             for (var i = 0; i < count; ++i)
@@ -167,9 +153,9 @@ namespace System.Data.Entity.Core.Objects.Internal
             return count;
         }
 
-        public override bool IsDBNull(int ordinal)
+        public bool IsDBNull(int ordinal)
         {
-            return DBNull.Value.Equals(_currentRow[ordinal]);
+            return DBNull.Value == _currentRow[ordinal];
         }
 
 #if !NET40
@@ -182,22 +168,22 @@ namespace System.Data.Entity.Core.Objects.Internal
 
 #endif
 
-        public override string GetDataTypeName(int ordinal)
+        public string GetDataTypeName(int ordinal)
         {
             return _dataTypeNames[ordinal];
         }
 
-        public override Type GetFieldType(int ordinal)
+        public Type GetFieldType(int ordinal)
         {
             return _fieldTypes[ordinal];
         }
 
-        public override string GetName(int ordinal)
+        public string GetName(int ordinal)
         {
             return _columnNames[ordinal];
         }
 
-        public override int GetOrdinal(string name)
+        public int GetOrdinal(string name)
         {
             return _fieldNameLookup.GetOrdinal(name);
         }
