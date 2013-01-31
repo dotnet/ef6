dotnet/ef6

commit c8dc3155b21648978008ae8c872f2e92c7b432be

AssociationSetAssociatedWithAssociationSetDeadEnd - adding properties to deal with different ordering of association and association set ends
In CodeFirst pipeline there is an assumptions that AssociationSet Ends
are in the same order as Association Ends. In general this does not have
to be true. Due to this assumption when dealing with arbitrary StoreItemCollections
SSDLSerializer would write an invalid SSDL. The fix is to add properties for
SourceEnd and TargetEnd so that they return ends based on the name and not on
the order and use those in the SSDL Serializer.

src/EntityFramework/Core/Metadata/Edm/AssociationSet.cs
@@ -100,6 +100,30 @@ namespace System.Data.Entity.Core.Metadata.Edm
             }
         }
 
+        internal AssociationEndMember SourceEnd
+        {
+            get
+            {
+                var associationSetEnd = AssociationSetEnds.FirstOrDefault();
+                return
+                    associationSetEnd != null
+                        ? ElementType.AssociationEndMembers.SingleOrDefault(e => e.Name == associationSetEnd.Name)
+                        : null;
+            }
+        }
+
+        internal AssociationEndMember TargetEnd
+        {
+            get
+            {
+                var associationSetEnd = AssociationSetEnds.ElementAtOrDefault(1);
+                return
+                    associationSetEnd != null
+                        ? ElementType.AssociationEndMembers.SingleOrDefault(e => e.Name == associationSetEnd.Name)
+                        : null;
+            }
+        }
+
         /// <summary>
         ///     Returns the kind of the type
         /// </summary>

test/EntityFramework/UnitTests/Core/Metadata/Edm/AssociationSetTests.cs
@@ -32,5 +32,31 @@ namespace System.Data.Entity.Core.Metadata.Edm
             Assert.Same(sourceEntitySet, associationSet.SourceSet);
             Assert.Same(targetEntitySet, associationSet.TargetSet);
         }
+
+        [Fact]
+        public void Can_get_ends_via_wrapper_properties()
+        {
+            var sourceEnd = new AssociationEndMember("S", new EntityType());
+            var targetEnd = new AssociationEndMember("T", new EntityType());
+
+            var associationType
+                = new AssociationType();
+
+            associationType.AddKeyMember(targetEnd);
+            associationType.AddKeyMember(sourceEnd);
+
+            var associationSet = new AssociationSet("A", associationType);
+
+            Assert.Null(associationSet.SourceEnd);
+            Assert.Null(associationSet.TargetEnd);
+
+            associationSet.AddAssociationSetEnd(new AssociationSetEnd(new EntitySet(), associationSet, sourceEnd));
+            associationSet.AddAssociationSetEnd(new AssociationSetEnd(new EntitySet(), associationSet, targetEnd));
+
+            associationType.SetReadOnly();
+
+            Assert.Same(sourceEnd, associationSet.SourceEnd);
+            Assert.Same(targetEnd, associationSet.TargetEnd);
+        }
     }
 }

src/EntityFramework/Edm/Serialization/EdmSerializationVisitor.cs
@@ -60,11 +60,11 @@ namespace System.Data.Entity.Edm.Serialization
             base.VisitEdmAssociationSet(item);
             if (item.SourceSet != null)
             {
-                _schemaWriter.WriteAssociationSetEndElement(item.SourceSet, item.ElementType.SourceEnd.Name);
+                _schemaWriter.WriteAssociationSetEndElement(item.SourceSet, item.SourceEnd.Name);
             }
             if (item.TargetSet != null)
             {
-                _schemaWriter.WriteAssociationSetEndElement(item.TargetSet, item.ElementType.TargetEnd.Name);
+                _schemaWriter.WriteAssociationSetEndElement(item.TargetSet, item.TargetEnd.Name);
             }
             _schemaWriter.WriteEndElement();
         }

commit eab1564f578c646dd189e264e6202b7a90212414

TheReturnOfTheAssert - fix for issue 839 - Test failures on debug build
The change c8dc3155b216 caused failure on debug build since it was calling into wrapper properties that in some asserted since the metadata was not set readonly. The fix is to directly call into MetadataWorkspace.
Reverted Assert removal
Successfully re-ran all the UnitTests on debug build.

src/EntityFramework/Core/Metadata/Edm/AssociationSet.cs
@@ -107,7 +107,7 @@ namespace System.Data.Entity.Core.Metadata.Edm
                 var associationSetEnd = AssociationSetEnds.FirstOrDefault();
                 return
                     associationSetEnd != null
-                        ? ElementType.AssociationEndMembers.SingleOrDefault(e => e.Name == associationSetEnd.Name)
+                        ? ElementType.Members.OfType<AssociationEndMember>().SingleOrDefault(e => e.Name == associationSetEnd.Name)
                         : null;
             }
         }
@@ -119,7 +119,7 @@ namespace System.Data.Entity.Core.Metadata.Edm
                 var associationSetEnd = AssociationSetEnds.ElementAtOrDefault(1);
                 return
                     associationSetEnd != null
-                        ? ElementType.AssociationEndMembers.SingleOrDefault(e => e.Name == associationSetEnd.Name)
+                        ? ElementType.Members.OfType<AssociationEndMember>().SingleOrDefault(e => e.Name == associationSetEnd.Name)
                         : null;
             }
         }

src/EntityFramework/Core/Metadata/Edm/AssociationType.cs
@@ -61,6 +61,10 @@ namespace System.Data.Entity.Core.Metadata.Edm
         {
             get
             {
+                Debug.Assert(
+                    IsReadOnly,
+                    "this is a wrapper around this.Members, don't call it during metadata loading, only call it after the metadata is set to read-only");
+            
                 if (null == _associationEndMembers)
                 {
                     Interlocked.CompareExchange(

test/EntityFramework/UnitTests/Core/Metadata/Edm/AssociationSetTests.cs
@@ -34,7 +34,7 @@ namespace System.Data.Entity.Core.Metadata.Edm
         }
 
         [Fact]
-        public void Can_get_ends_via_wrapper_properties()
+        public void Can_get_association_ends_from_association_set()
         {
             var sourceEnd = new AssociationEndMember("S", new EntityType());
             var targetEnd = new AssociationEndMember("T", new EntityType());
@@ -52,8 +52,6 @@ namespace System.Data.Entity.Core.Metadata.Edm
 
             associationSet.AddAssociationSetEnd(new AssociationSetEnd(new EntitySet(), associationSet, sourceEnd));
             associationSet.AddAssociationSetEnd(new AssociationSetEnd(new EntitySet(), associationSet, targetEnd));
-
-            associationType.SetReadOnly();
 
             Assert.Same(sourceEnd, associationSet.SourceEnd);
             Assert.Same(targetEnd, associationSet.TargetEnd);