dotnet/ef6

commit f15fb3b6614e9dee5979e2698274dc766848e06c

Fix for #849 - Code First pipeline errors with recent builds
Issue: For collections having greater than 25 elements, MetadataCollection<T> uses an internal dictionary for fast lookup by item identity. Using Code First, this dictionary can become out of sync because on the CF code path item identity is mutable during the model construction phase.

Solution: Instead of maintaining the fast-lookup dictionary on the fly, build it once after the build phase when the collection goes read only.

src/EntityFramework/Core/Metadata/Edm/MetadataCollection.cs
@@ -130,7 +130,6 @@ namespace System.Data.Entity.Core.Metadata.Edm
                 ThrowIfReadOnly();
 
                 _collectionData.OrderedList[index] = value;
-                _collectionData.IdentityDictionary = null;
             }
         }
 
@@ -186,7 +185,6 @@ namespace System.Data.Entity.Core.Metadata.Edm
             ThrowIfReadOnly();
 
             _collectionData.OrderedList.Remove(item);
-            _collectionData.IdentityDictionary = null;
         }
 
         /// <summary>
@@ -318,6 +316,7 @@ namespace System.Data.Entity.Core.Metadata.Edm
             {
                 // We only have to take care of the ordered list.
                 index = IndexOf(collectionData, item.Identity, false);
+
                 if (0 <= index)
                 {
                     // The item is found in the linear ordered list. Unless
@@ -325,24 +324,6 @@ namespace System.Data.Entity.Core.Metadata.Edm
                     if (!updateIfFound)
                     {
                         throw new ArgumentException(Strings.ItemDuplicateIdentity(item.Identity), "item", null);
-                    }
-                }
-                else
-                {
-                    // This is a new item to be inserted. Grow if we must before adding to ordered list.
-                    if (UseSortedListCrossover <= listCount)
-                    {
-                        collectionData.IdentityDictionary
-                            = new Dictionary<string, OrderedIndex>(
-                                collectionData.OrderedList.Count + 1,
-                                StringComparer.OrdinalIgnoreCase);
-
-                        for (var i = 0; i < collectionData.OrderedList.Count; ++i)
-                        {
-                            AddToDictionary(collectionData, collectionData.OrderedList[i].Identity, i, false);
-                        }
-
-                        AddToDictionary(collectionData, item.Identity, listCount, false);
                     }
                 }
             }
@@ -357,6 +338,7 @@ namespace System.Data.Entity.Core.Metadata.Edm
             else
             {
                 Debug.Assert(index == -1 || index == listCount);
+
                 collectionData.OrderedList.Add(item);
             }
         }
@@ -636,8 +618,26 @@ namespace System.Data.Entity.Core.Metadata.Edm
             {
                 _collectionData.OrderedList[i].SetReadOnly();
             }
+
             _collectionData.OrderedList.TrimExcess();
+
+            // Build the fast-access by identity dictionary
+            if ((_collectionData.IdentityDictionary == null)
+                && UseSortedListCrossover <= _collectionData.OrderedList.Count)
+            {
+                _collectionData.IdentityDictionary
+                    = new Dictionary<string, OrderedIndex>(
+                        _collectionData.OrderedList.Count,
+                        StringComparer.OrdinalIgnoreCase);
+
+                for (var i = 0; i < _collectionData.OrderedList.Count; ++i)
+                {
+                    AddToDictionary(_collectionData, _collectionData.OrderedList[i].Identity, i, false);
+                }
+            }
+
             _readOnly = true;
+
             return this;
         }
 

commit 7a17a2d60eccdee9072eb6edef89d2f4929dba8e

Fix for #902: General performance regression introduced by changeset f15fb3b6614e
Problem: In fixing #849, perf regressed because certain metadata collections never go read-only and so the fast-lookup-by-identity dictionary was no longer being built.

Solution: Revert the fix for #849 and change MetadataCollection so that it invalidates the internal cache when, either:
1) An item is removed.
2) An item identity is changed (currently we only need to worry about EntitySet and EdmMember).

The identity cache is now built lazily so that it is only built/rebuilt the first time it is needed.

Verification: David has tested this change with various regressed perf tests and determined that we are now either as fast or faster than before the regression.

src/EntityFramework/Core/Metadata/Edm/EdmMember.cs
@@ -56,16 +56,24 @@ namespace System.Data.Entity.Core.Metadata.Edm
                 Check.NotEmpty(value, "value");
                 Util.ThrowIfReadOnly(this);
 
-                _name = value;
-
-                if ((_declaringType != null)
-                    && (_declaringType.Members.Except(new[] { this })
-                                      .Any(c => string.Equals(Identity, c.Identity, StringComparison.Ordinal))))
+                if (!string.Equals(_name, value, StringComparison.Ordinal))
                 {
-                    // Duplicate configured name, uniquify the identity so that
-                    // a validation exception can be generated later on. For valid
-                    // models, we sync it back up in SetReadOnly()
-                    _identity = _declaringType.Members.UniquifyName(Identity);
+                    _name = value;
+
+                    if (_declaringType != null)
+                    {
+                        if (_declaringType
+                            .Members.Except(new[] { this })
+                            .Any(c => string.Equals(Identity, c.Identity, StringComparison.Ordinal)))
+                        {
+                            // Duplicate configured name, uniquify the identity so that
+                            // a validation exception can be generated later on. For valid
+                            // models, we sync it back up in SetReadOnly()
+                            _identity = _declaringType.Members.UniquifyName(Identity);
+                        }
+
+                        _declaringType.NotifyItemIdentityChanged();
+                    }
                 }
             }
         }

src/EntityFramework/Core/Metadata/Edm/EntityContainer.cs
@@ -17,6 +17,11 @@ namespace System.Data.Entity.Core.Metadata.Edm
         private readonly ReadOnlyMetadataCollection<EntitySetBase> _baseEntitySets;
         private readonly ReadOnlyMetadataCollection<EdmFunction> _functionImports;
 
+        internal EntityContainer()
+        {
+            // mocking only
+        }
+
         /// <summary>
         ///     The constructor for constructing the EntityContainer object with the name, namespaceName, and version.
         /// </summary>
@@ -261,6 +266,11 @@ namespace System.Data.Entity.Core.Metadata.Edm
             entityContainer.SetReadOnly();
 
             return entityContainer;
+        }
+
+        internal virtual void NotifyItemIdentityChanged()
+        {
+            _baseEntitySets.Source.InvalidateCache();
         }
     }
 }

src/EntityFramework/Core/Metadata/Edm/EntitySetBase.cs
@@ -110,7 +110,15 @@ namespace System.Data.Entity.Core.Metadata.Edm
                 Check.NotEmpty(value, "value");
                 Util.ThrowIfReadOnly(this);
 
-                _name = value;
+                if (!string.Equals(_name, value, StringComparison.Ordinal))
+                {
+                    _name = value;
+
+                    if (_entityContainer != null)
+                    {
+                        _entityContainer.NotifyItemIdentityChanged();
+                    }
+                }
             }
         }
 

src/EntityFramework/Core/Metadata/Edm/EntityTypeBase.cs
@@ -179,5 +179,12 @@ namespace System.Data.Entity.Core.Metadata.Edm
 
             base.RemoveMember(member);
         }
+
+        internal override void NotifyItemIdentityChanged()
+        {
+            base.NotifyItemIdentityChanged();
+
+            _keyMembers.Source.InvalidateCache();
+        }
     }
 }

src/EntityFramework/Core/Metadata/Edm/MetadataCollection.cs
@@ -128,6 +128,7 @@ namespace System.Data.Entity.Core.Metadata.Edm
             set
             {
                 ThrowIfReadOnly();
+                Debug.Assert(_collectionData.IdentityDictionary.Value == null);
 
                 _collectionData.OrderedList[index] = value;
             }
@@ -185,6 +186,40 @@ namespace System.Data.Entity.Core.Metadata.Edm
             ThrowIfReadOnly();
 
             _collectionData.OrderedList.Remove(item);
+
+            InvalidateCache();
+        }
+
+        public void InvalidateCache()
+        {
+            _collectionData.IdentityDictionary 
+                = new Lazy<Dictionary<string, OrderedIndex>>(
+                () =>
+                    {
+                        if (_collectionData.OrderedList.Count > UseSortedListCrossover)
+                        {
+                            // Rebuild the fast by-identity lookup dictionary
+
+                            var identityDictionary
+                                = new Dictionary<string, OrderedIndex>(
+                                    _collectionData.OrderedList.Count,
+                                    StringComparer.OrdinalIgnoreCase);
+
+                            for (var i = 0; i < _collectionData.OrderedList.Count; i++)
+                            {
+                                AddToDictionary(
+                                    identityDictionary,
+                                    _collectionData.OrderedList,
+                                    _collectionData.OrderedList[i].Identity,
+                                    i,
+                                    false);
+                            }
+
+                            return identityDictionary;
+                        }
+
+                        return null;
+                    });
         }
 
         /// <summary>
@@ -196,10 +231,15 @@ namespace System.Data.Entity.Core.Metadata.Edm
         /// <param name="updateIfFound"> Whether the item should be updated if a matching item is found. </param>
         /// <returns> Index of the added entity, possibly different from the index parameter if updateIfFound is true. </returns>
         [SuppressMessage("Microsoft.Usage", "CA2208:InstantiateArgumentExceptionsCorrectly")]
-        private static int AddToDictionary(CollectionData collectionData, string identity, int index, bool updateIfFound)
+        private static int AddToDictionary(
+            Dictionary<string, OrderedIndex> identityDictionary,
+            List<T> orderedList,
+            string identity,
+            int index,
+            bool updateIfFound)
         {
-            DebugCheck.NotNull(collectionData);
-            DebugCheck.NotNull(collectionData.IdentityDictionary);
+            DebugCheck.NotNull(identityDictionary);
+            DebugCheck.NotNull(orderedList);
             DebugCheck.NotEmpty(identity);
 
             int[] inexact = null;
@@ -207,10 +247,10 @@ namespace System.Data.Entity.Core.Metadata.Edm
             var exactIndex = index;
 
             // find the item(s) by OrdinalIgnoreCase
-            if (collectionData.IdentityDictionary.TryGetValue(identity, out orderIndex))
+            if (identityDictionary.TryGetValue(identity, out orderIndex))
             {
                 // identity was already tracking an item, verify its not a duplicate by exact name
-                if (EqualIdentity(collectionData.OrderedList, orderIndex.ExactIndex, identity))
+                if (EqualIdentity(orderedList, orderIndex.ExactIndex, identity))
                 {
                     // If the item is already here and we are updating, there is no more work to be done.
                     if (updateIfFound)
@@ -227,7 +267,7 @@ namespace System.Data.Entity.Core.Metadata.Edm
                     // identity was already tracking multiple items, verify its not a duplicate by exact name
                     for (var i = 0; i < orderIndex.InexactIndexes.Length; ++i)
                     {
-                        if (EqualIdentity(collectionData.OrderedList, orderIndex.InexactIndexes[i], identity))
+                        if (EqualIdentity(orderedList, orderIndex.InexactIndexes[i], identity))
                         {
                             // If the item is already here and we are updating, there is no more work to be done.
                             if (updateIfFound)
@@ -253,7 +293,7 @@ namespace System.Data.Entity.Core.Metadata.Edm
             }
             // else this is a new identity
 
-            collectionData.IdentityDictionary[identity] = new OrderedIndex(exactIndex, inexact);
+            identityDictionary[identity] = new OrderedIndex(exactIndex, inexact);
 
             return index;
         }
@@ -308,15 +348,20 @@ namespace System.Data.Entity.Core.Metadata.Edm
 
             int index;
             var listCount = collectionData.OrderedList.Count;
-            if (null != collectionData.IdentityDictionary)
+            if (null != collectionData.IdentityDictionary.Value)
             {
-                index = AddToDictionary(collectionData, item.Identity, listCount, updateIfFound);
+                index
+                    = AddToDictionary(
+                        collectionData.IdentityDictionary.Value,
+                        collectionData.OrderedList,
+                        item.Identity,
+                        listCount,
+                        updateIfFound);
             }
             else
             {
                 // We only have to take care of the ordered list.
                 index = IndexOf(collectionData, item.Identity, false);
-
                 if (0 <= index)
                 {
                     // The item is found in the linear ordered list. Unless
@@ -324,6 +369,34 @@ namespace System.Data.Entity.Core.Metadata.Edm
                     if (!updateIfFound)
                     {
                         throw new ArgumentException(Strings.ItemDuplicateIdentity(item.Identity), "item", null);
+                    }
+                }
+                else
+                {
+                    // This is a new item to be inserted. Grow if we must before adding to ordered list.
+                    if (UseSortedListCrossover <= listCount)
+                    {
+                        collectionData.IdentityDictionary
+                            = new Lazy<Dictionary<string, OrderedIndex>>(
+                                () =>
+                                    {
+                                        var identityDictionary
+                                            = new Dictionary<string, OrderedIndex>(
+                                                collectionData.OrderedList.Count + 1,
+                                                StringComparer.OrdinalIgnoreCase);
+
+                                        for (var i = 0; i < collectionData.OrderedList.Count; ++i)
+                                        {
+                                            AddToDictionary(
+                                                identityDictionary,
+                                                collectionData.OrderedList,
+                                                collectionData.OrderedList[i].Identity,
+                                                i,
+                                                false);
+                                        }
+
+                                        return identityDictionary;
+                                    });
                     }
                 }
             }
@@ -338,7 +411,6 @@ namespace System.Data.Entity.Core.Metadata.Edm
             else
             {
                 Debug.Assert(index == -1 || index == listCount);
-
                 collectionData.OrderedList.Add(item);
             }
         }
@@ -469,11 +541,11 @@ namespace System.Data.Entity.Core.Metadata.Edm
             DebugCheck.NotNull(identity);
 
             var index = -1;
-            if (null != collectionData.IdentityDictionary)
+            if (null != collectionData.IdentityDictionary.Value)
             {
                 // OrdinalIgnoreCase dictionary lookup
                 OrderedIndex orderIndex;
-                if (collectionData.IdentityDictionary.TryGetValue(identity, out orderIndex))
+                if (collectionData.IdentityDictionary.Value.TryGetValue(identity, out orderIndex))
                 {
                     if (ignoreCase)
                     {
@@ -618,26 +690,8 @@ namespace System.Data.Entity.Core.Metadata.Edm
             {
                 _collectionData.OrderedList[i].SetReadOnly();
             }
-
             _collectionData.OrderedList.TrimExcess();
-
-            // Build the fast-access by identity dictionary
-            if ((_collectionData.IdentityDictionary == null)
-                && UseSortedListCrossover <= _collectionData.OrderedList.Count)
-            {
-                _collectionData.IdentityDictionary
-                    = new Dictionary<string, OrderedIndex>(
-                        _collectionData.OrderedList.Count,
-                        StringComparer.OrdinalIgnoreCase);
-
-                for (var i = 0; i < _collectionData.OrderedList.Count; ++i)
-                {
-                    AddToDictionary(_collectionData, _collectionData.OrderedList[i].Identity, i, false);
-                }
-            }
-
             _readOnly = true;
-
             return this;
         }
 
@@ -695,7 +749,8 @@ namespace System.Data.Entity.Core.Metadata.Edm
             ///     an OrderedIndex structure with other case-insensitive matches for the
             ///     entry.  See additional comments in AddInternal.
             /// </summary>
-            internal Dictionary<string, OrderedIndex> IdentityDictionary;
+            internal Lazy<Dictionary<string, OrderedIndex>> IdentityDictionary
+                = new Lazy<Dictionary<string, OrderedIndex>>(() => null);
 
             internal readonly List<T> OrderedList;
 
@@ -715,22 +770,32 @@ namespace System.Data.Entity.Core.Metadata.Edm
 
                 if (UseSortedListCrossover <= OrderedList.Capacity)
                 {
-                    IdentityDictionary = new Dictionary<string, OrderedIndex>(OrderedList.Capacity, StringComparer.OrdinalIgnoreCase);
+                    IdentityDictionary
+                        = new Lazy<Dictionary<string, OrderedIndex>>(
+                            () =>
+                                {
+                                    var identityDictionary
+                                        = new Dictionary<string, OrderedIndex>(
+                                            OrderedList.Capacity,
+                                            StringComparer.OrdinalIgnoreCase);
 
-                    if (null != original.IdentityDictionary)
-                    {
-                        foreach (var pair in original.IdentityDictionary)
-                        {
-                            IdentityDictionary.Add(pair.Key, pair.Value);
-                        }
-                    }
-                    else
-                    {
-                        for (var i = 0; i < OrderedList.Count; ++i)
-                        {
-                            AddToDictionary(this, OrderedList[i].Identity, i, false);
-                        }
-                    }
+                                    if (null != original.IdentityDictionary.Value)
+                                    {
+                                        foreach (var pair in original.IdentityDictionary.Value)
+                                        {
+                                            identityDictionary.Add(pair.Key, pair.Value);
+                                        }
+                                    }
+                                    else
+                                    {
+                                        for (var i = 0; i < OrderedList.Count; ++i)
+                                        {
+                                            AddToDictionary(identityDictionary, OrderedList, OrderedList[i].Identity, i, false);
+                                        }
+                                    }
+
+                                    return identityDictionary;
+                                });
                 }
             }
         }

src/EntityFramework/Core/Metadata/Edm/StructuralType.cs
@@ -115,5 +115,10 @@ namespace System.Data.Entity.Core.Metadata.Edm
 
             _members.Remove(member);
         }
+
+        internal virtual void NotifyItemIdentityChanged()
+        {
+            _members.InvalidateCache();
+        }
     }
 }

test/EntityFramework/UnitTests/Core/Metadata/Edm/EdmMemberTests.cs
@@ -2,6 +2,7 @@
 
 namespace System.Data.Entity.Core.Metadata.Edm
 {
+    using Moq;
     using Xunit;
 
     public class EdmMemberTests
@@ -16,6 +17,22 @@ namespace System.Data.Entity.Core.Metadata.Edm
             property.Name = "Foo";
 
             Assert.Equal("Foo", property.Name);
+        }
+
+        [Fact]
+        public void Can_set_name_and_parent_notified()
+        {
+            var primitiveType = PrimitiveType.GetEdmPrimitiveType(PrimitiveTypeKind.Int32);
+
+            var property = EdmProperty.Primitive("P", primitiveType);
+
+            var entityTypeMock = new Mock<StructuralType>();
+
+            property.ChangeDeclaringTypeWithoutCollectionFixup(entityTypeMock.Object);
+
+            property.Name = "Foo";
+
+            entityTypeMock.Verify(e => e.NotifyItemIdentityChanged(), Times.Once());
         }
 
         [Fact]

test/EntityFramework/UnitTests/Core/Metadata/Edm/EntityContainerTests.cs
@@ -56,7 +56,7 @@ namespace System.Data.Entity.Core.Metadata.Edm
         [Fact]
         public void Create_factory_method_sets_properties_and_seals_the_type()
         {
-            var entitySets = new[] { new EntitySet() { Name = "Bar"} };
+            var entitySets = new[] { new EntitySet { Name = "Bar"} };
 
             var entityContainer = 
                 EntityContainer.Create("Foo", DataSpace.SSpace, entitySets, null);

test/EntityFramework/UnitTests/Core/Metadata/Edm/EntitySetBaseTests.cs
@@ -2,6 +2,7 @@
 
 namespace System.Data.Entity.Core.Metadata.Edm
 {
+    using Moq;
     using Xunit;
 
     public class EntitySetBaseTests
@@ -20,6 +21,20 @@ namespace System.Data.Entity.Core.Metadata.Edm
                       };
 
             Assert.Equal("Foo", entitySetBase.Name);
+        }
+
+        [Fact]
+        public void Can_set_name_and_parent_notified()
+        {
+            var entitySetBase = new TestEntitySetBase();
+
+            var entityContainerMock = new Mock<EntityContainer>();
+
+            entitySetBase.ChangeEntityContainerWithoutCollectionFixup(entityContainerMock.Object);
+
+            entitySetBase.Name = "Foo";
+
+            entityContainerMock.Verify(e => e.NotifyItemIdentityChanged(), Times.Once());
         }
 
         [Fact]

test/EntityFramework/UnitTests/Core/Metadata/Edm/MetadataCollectionTests.cs
@@ -25,7 +25,30 @@ namespace System.Data.Entity.Core.Metadata.Edm
         }
 
         [Fact]
-        public void Can_replace_value_via_ordinal_indexer()
+        public void Can_remove_item_from_collection_and_identity_dictionary_updated()
+        {
+            var metadataCollection = new MetadataCollection<EntityType>();
+
+            for (var i = 0; i < 30; i++)
+            {
+                metadataCollection.Add(new EntityType("E" + i, "N", DataSpace.CSpace));
+            }
+
+            for (var i = 0; i < metadataCollection.Count; i++)
+            {
+                // This will throw if the identity cache gets out of sync.
+
+                var entityType = metadataCollection[i];
+
+                metadataCollection.Remove(entityType);
+                metadataCollection.Add(entityType);
+            }
+
+            Assert.Equal(30, metadataCollection.Count);
+        }
+
+        [Fact]
+        public void Can_replace_item_via_ordinal_indexer()
         {
             var metadataCollection = new MetadataCollection<EntityType>();
 
@@ -51,7 +74,11 @@ namespace System.Data.Entity.Core.Metadata.Edm
             Assert.Equal(
                 Strings.ADP_CollectionParameterElementIsNull("items"),
                 Assert.Throws<ArgumentException>(
-                    () => metadataCollection.AtomicAddRange(new List<MetadataItem>{ null })).Message);
+                    () => metadataCollection.AtomicAddRange(
+                        new List<MetadataItem>
+                            {
+                                null
+                            })).Message);
         }
     }
 }