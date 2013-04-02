15e7303c4a216def176c2eef4fca17013c89d314
unknown <daobando@daobando-w3.redmond.corp.microsoft.com>
2013-04-02 14:10:22 -0700

    Performance: MemberCollection.GetDeclaredOnlyMembers<T>() is now using a List<T> instead of a MetadataCollection<T> as the underlying data for the ReadOnlyMetadataCollection<T> that it returns. This is safe since the List<T> is being populated from an already-sorted MetadataCollection<T>.

--- src/EntityFramework/Core/Metadata/Edm/MemberCollection.cs
+++ src/EntityFramework/Core/Metadata/Edm/MemberCollection.cs
@@ -251,7 +251,7 @@ namespace System.Data.Entity.Core.Metadata.Edm
         /// </summary>
         internal ReadOnlyMetadataCollection<T> GetDeclaredOnlyMembers<T>() where T : EdmMember
         {
-            var newCollection = new MetadataCollection<T>();
+            var newCollection = new List<T>();
             for (var i = 0; i < base.Count; i++)
             {
                 var member = base[i] as T;
@@ -261,7 +261,7 @@ namespace System.Data.Entity.Core.Metadata.Edm
                 }
             }
 
-            return newCollection.AsReadOnlyMetadataCollection();
+            return new ReadOnlyMetadataCollection<T>(newCollection);
         }
 
         /// <summary>
