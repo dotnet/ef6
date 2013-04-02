a2444bf8acbf7fa64725e157ab477550c5c8f0d1
unknown <daobando@daobando-w3.redmond.corp.microsoft.com>
2013-04-01 22:26:57 -0700

    GroupTherapy - fixes Dev11 bug 448362 "GroupBy statements in Linq to Entities are not expanded appropriately". The problem is caused by the inappropriate handling of the NewRecordOp in the IQT during the AggregatePushdown processing phase. The GroupAggregateVarComputationTranslator was forcing AggregatePushdown to ignore NewRecordOp (by overriding the visitor and returning null) instead of letting it be processed normally, as in Dev10. This bug was introduced by Dev10 changeset 1871382, which is a fix to Dev10 bug 909964 "Null ref exception in NTE for keyless GroupPartition query". However, keyless GroupPartition queries were never released as a supported feature and instead broke the functionality of group by statements that made use of compound grouping keys. This change also includes two new functional tests for the aggregate pushdown of group by statements in Linq expressions.

---
+++ test/EntityFramework/FunctionalTests/PlanCompiler/LinqGroupByTests.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.   
+
+namespace PlanCompilerTests
+{
+    using System.Data.Entity;
+    using System.Linq;
+    using AdvancedPatternsModel;
+    using Xunit;
+
+    /// <summary>   
+    ///     Tests for GroupBy statements in Linq queries.   
+    /// </summary>   
+    public class LinqGroupByTests : FunctionalTestBase
+    {
+        #region Infrastructure/setup
+
+        static LinqGroupByTests()
+        {
+        }
+
+        #endregion
+
+        #region Tests for GroupBy that trigger an aggregate pushdown
+
+        [Fact]
+        private void GroupBy_aggregate_pushdown_single_key()
+        {
+            using (var context = new AdvancedPatternsMasterContext())
+            {
+                var groupByQuery = from workOrder in context.WorkOrders
+                                   group new { workOrder.WorkOrderId, workOrder.Details } by workOrder.EmployeeId into ordersByEmployeeGroup
+                                   select new
+                                   {
+                                       EmployeeId = ordersByEmployeeGroup.Key,
+                                       OrderCount = ordersByEmployeeGroup.Count(),
+                                       MaxOrderId = ordersByEmployeeGroup.Max(o => o.WorkOrderId)
+                                   };
+                var sql = groupByQuery.ToString();
+                Assert.True(sql != null && sql.ToUpper().Contains("GROUP BY"));
+            }
+        }
+
+        // Dev11 448362   
+        [Fact]
+        private void GroupBy_aggregate_pushdown_translates_NewRecordOp()
+        {
+            using (var context = new AdvancedPatternsMasterContext())
+            {
+                var groupByNewQuery = from workOrder in context.WorkOrders
+                                      group new { workOrder.WorkOrderId, workOrder.Details } by new { workOrder.EmployeeId } into ordersByEmployeeGroup
+                                      select new
+                                      {
+                                          ordersByEmployeeGroup.Key.EmployeeId,
+                                          OrderCount = ordersByEmployeeGroup.Count(),
+                                          MaxOrderId = ordersByEmployeeGroup.Max(o => o.WorkOrderId)
+                                      };
+                var sql = groupByNewQuery.ToString();
+                Assert.True(sql != null && sql.ToUpper().Contains("GROUP BY"));
+            }
+        }
+
+        #endregion
+    }
+}
