859fe7b822c927d8fd42d890cdd3e0569f5cd22f
Brice Lambson <bricelam@microsoft.com>
2013-08-13 11:59:38 -0700

    Unifying assembly metadata.
    Work Item: 1518

---
+++ src/EntityFramework.SqlServer/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+using System.Reflection;
+
+[assembly: AssemblyTitle("EntityFramework.SqlServer")]
+[assembly: AssemblyDescription("EntityFramework.SqlServer.dll")]
+[assembly: AssemblyDefaultAlias("EntityFramework.SqlServer.dll")]
