43083547f89ec9b149a71995dbcfa701714fe3e1
AndrewPeters <anpete@microsoft.com>
2013-08-23 14:04:55 -0700

    Fixed bad code formatting.

--- test/EntityFramework/FunctionalTests/CodeFirst/ConfigurationScenarioTests.cs
+++ test/EntityFramework/FunctionalTests/CodeFirst/ConfigurationScenarioTests.cs
@@ -19,32 +19,32 @@ namespace System.Data.Entity.CodeFirst
             }
         }
 
-            public class E
-            {
-                public int Id { get; set; }
-                public decimal P1 { get; set; }
-                public TimeSpan P2 { get; set; }
-                public DateTime P3 { get; set; }
-                public DateTimeOffset P4 { get; set; }
-            }
+        public class E
+        {
+            public int Id { get; set; }
+            public decimal P1 { get; set; }
+            public TimeSpan P2 { get; set; }
+            public DateTime P3 { get; set; }
+            public DateTimeOffset P4 { get; set; }
+        }
 
-            [Fact]
-            public void Default_precision_facets_are_not_null_within_store_model()
-            {
-                var modelBuilder = new DbModelBuilder();
+        [Fact]
+        public void Default_precision_facets_are_not_null_within_store_model()
+        {
+            var modelBuilder = new DbModelBuilder();
 
-                modelBuilder.Entity<E>();
-                modelBuilder.Properties<DateTime>().Configure(p => p.HasColumnType("datetime2"));
+            modelBuilder.Entity<E>();
+            modelBuilder.Properties<DateTime>().Configure(p => p.HasColumnType("datetime2"));
 
-                var model = modelBuilder.Build(ProviderRegistry.Sql2008_ProviderInfo);
+            var model = modelBuilder.Build(ProviderRegistry.Sql2008_ProviderInfo);
 
-                var storeModel = ((IEdmModelAdapter)model).StoreModel;
-                var entityType = storeModel.EntityTypes.ElementAt(0);
+            var storeModel = ((IEdmModelAdapter)model).StoreModel;
+            var entityType = storeModel.EntityTypes.ElementAt(0);
 
-                Assert.Equal(18, (byte)entityType.DeclaredProperties["P1"].TypeUsage.Facets["Precision"].Value);
-                Assert.Equal(7, (byte)entityType.DeclaredProperties["P2"].TypeUsage.Facets["Precision"].Value);
-                Assert.Equal(7, (byte)entityType.DeclaredProperties["P3"].TypeUsage.Facets["Precision"].Value);
-                Assert.Equal(7, (byte)entityType.DeclaredProperties["P4"].TypeUsage.Facets["Precision"].Value);
-            }
+            Assert.Equal(18, (byte)entityType.DeclaredProperties["P1"].TypeUsage.Facets["Precision"].Value);
+            Assert.Equal(7, (byte)entityType.DeclaredProperties["P2"].TypeUsage.Facets["Precision"].Value);
+            Assert.Equal(7, (byte)entityType.DeclaredProperties["P3"].TypeUsage.Facets["Precision"].Value);
+            Assert.Equal(7, (byte)entityType.DeclaredProperties["P4"].TypeUsage.Facets["Precision"].Value);
+        }
     }
 }
