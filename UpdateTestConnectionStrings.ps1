4c79de162986280b80dfa4fcde87183a3b727ba2
lukew <lukew@microsoft.com>
2013-09-11 11:24:53 -0700

    Added a script to update the connection strings for all of the test projects

---
+++ UpdateTestConnectionStrings.ps1
@@ -0,0 +1,32 @@
+# Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+Param(
+  [string]$newConnectionString
+)
+
+foreach($appConfigPath in "FunctionalTests", "FunctionalTests.Transitional", "UnitTests", "VBTests")
+{
+	$appConfigPath = "$pwd\test\EntityFramework\$appConfigPath\App.config"
+	$xml = [xml] (get-content $appConfigPath)
+	$connectionStrings = $xml.configuration.connectionStrings.add
+	foreach ($conStr in $connectionStrings)
+	{
+		$str = $conStr.connectionString
+		if ($str.Contains($sqlexpress))
+		{
+			$str = $str.replace("Integrated Security=True;", "")
+			$str = $str.replace("Trusted_Connection=True;", "")
+			$str = $str.replace("Server=.\SQLEXPRESS;", "$newConnectionString")
+			$str = $str.replace("DataSource=.\SQLEXPRESS;", "$newConnectionString")
+		}
+		$conStr.connectionString = $str
+	}
+	foreach ($setting in $xml.configuration.appSettings.add)
+	{
+		if ($setting.key -eq "BaseConnectionString")
+		{
+			$setting.value="$newConnectionString"
+		}
+	}
+	$xml.Save("$appConfigPath")
+}
+
