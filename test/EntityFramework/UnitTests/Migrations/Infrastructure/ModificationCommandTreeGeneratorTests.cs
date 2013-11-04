dotnet/ef6

commit 9a111e568ceab661362dc3bd61ba41bd7a4e9660

CF/Migrations: Fix for #1101: Scaffolding of stored procedures fails with DbUpdateException for model with self reference and store generated key
- Problem: The update pipeline throws on insert for self-referencing entities that have a store-generated key when one end of the association is required. This manifests in a confusing way when we attempt to generate modification command trees during sproc body scaffolding.

- Solutions:

1) Improve the robustness of the ModificationCommandTreeGenerator to better deal with self-refs.
2) Change the StoreGeneratedIdentityKeyConvention to not introduce identity for self-refs where one end of the association is required. This is potential breaking change in that we will now generate slightly different EDM/DDL for a given OSpace. I discussed this with several folks and we agreed that this case is likely uncommon enough that we don't need to version it. NB. The model previously generated would fail on insert at runtime.
3) Wrap any UpdateException thrown during modification command tree generation to make the exception less confusing.

src/EntityFramework/Migrations/Infrastructure/ModificationCommandTreeGenerator.cs
@@ -4,11 +4,14 @@ namespace System.Data.Entity.Migrations.Infrastructure
 {
     using System.Collections.Generic;
     using System.Data.Common;
+    using System.Data.Entity.Core;
     using System.Data.Entity.Core.Common.CommandTrees;
     using System.Data.Entity.Core.Metadata.Edm;
+    using System.Data.Entity.Core.Objects;
     using System.Data.Entity.Infrastructure;
     using System.Data.Entity.Internal;
     using System.Data.Entity.ModelConfiguration.Edm;
+    using System.Data.Entity.Resources;
     using System.Data.Entity.Utilities;
     using System.Linq;
 
@@ -165,14 +168,25 @@ namespace System.Data.Entity.Migrations.Infrastructure
                 InstantiateComplexProperties(entity, entityType.Properties);
 
                 set.Attach(entity);
-                context.Entry(entity).State = state;
+
+                if (state != EntityState.Deleted)
+                {
+                    // For deletes, we need to set the state
+                    // _after_ dealing with IAs.
+                    context.Entry(entity).State = state;
+                }
 
                 ChangeRelationshipStates(context, entityType, entity, state);
 
+                if (state == EntityState.Deleted)
+                {
+                    context.Entry(entity).State = state;
+                }
+
                 using (var commandTracer = new CommandTracer(context))
                 {
-                    context.SaveChanges();
-
+                    ((IObjectContextAdapter)context).ObjectContext.SaveChanges(SaveOptions.None);
+                    
                     foreach (var commandTree in commandTracer.CommandTrees)
                     {
                         yield return (TCommandTree)commandTree;
@@ -227,7 +241,9 @@ namespace System.Data.Entity.Migrations.Infrastructure
 
                     var principalStub = set.Local.Cast<object>().SingleOrDefault();
 
-                    if (principalStub == null)
+                    if ((principalStub == null)
+                        || (ReferenceEquals(entity, principalStub)
+                            && state == EntityState.Added))
                     {
                         principalStub = set.Create();
 
@@ -262,6 +278,7 @@ namespace System.Data.Entity.Migrations.Infrastructure
                     case PrimitiveTypeKind.String:
                         clrPropertyInfo.SetValue(entity, "tmp", null);
                         break;
+
                     case PrimitiveTypeKind.Binary:
                         clrPropertyInfo.SetValue(entity, new byte[0], null);
                         break;

src/EntityFramework/ModelConfiguration/Conventions/Edm/StoreGeneratedIdentityKeyConvention.cs
@@ -40,12 +40,25 @@ namespace System.Data.Entity.ModelConfiguration.Conventions
                     && _applicableTypes.Contains(property.PrimitiveType.PrimitiveTypeKind))
                 {
                     if (!model.AssociationTypes.Any(a => IsNonTableSplittingForeignKey(a, property))
-                        && !ParentOfTpc(edmDataModelItem, model))
+                        && !ParentOfTpc(edmDataModelItem, model)
+                        && !HasRequiredSelfRef(edmDataModelItem, model))
                     {
                         property.SetStoreGeneratedPattern(StoreGeneratedPattern.Identity);
                     }
                 }
             }
+        }
+
+        private static bool HasRequiredSelfRef(EntityType entityType, EdmModel model)
+        {
+            DebugCheck.NotNull(entityType);
+            DebugCheck.NotNull(model);
+
+            return model.AssociationTypes
+                .Any(
+                    at => at.SourceEnd.GetEntityType().GetRootType() == entityType.GetRootType()
+                          && at.TargetEnd.GetEntityType().GetRootType() == entityType.GetRootType()
+                          && (at.SourceEnd.IsRequired() || at.TargetEnd.IsRequired()));
         }
 
         /// <summary>

test/EntityFramework/UnitTests/Migrations/Infrastructure/EdmModelDifferTests.cs
@@ -24,6 +24,49 @@ namespace System.Data.Entity.Migrations.Infrastructure
     [Variant(DatabaseProvider.SqlServerCe, ProgrammingLanguage.CSharp)]
     public class EdmModelDifferTests : DbTestCase
     {
+        public class WorldContext_Invalid : ModificationCommandTreeGeneratorTests.WorldContext_Fk
+        {
+            static WorldContext_Invalid()
+            {
+                Database.SetInitializer<WorldContext_Invalid>(null);
+            }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+
+                modelBuilder
+                    .Entity<ModificationCommandTreeGeneratorTests.Thing>()
+                    .Property(t => t.Id)
+                    .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            }
+        }
+
+        [MigrationsTheory]
+        public void Update_exceptions_should_be_wrapped_when_generating_sproc_bodies()
+        {
+            var modelBuilder = new DbModelBuilder();
+
+            var model1 = modelBuilder.Build(ProviderInfo);
+
+            var context = new WorldContext_Invalid();
+
+            var commandTreeGenerator
+                = new ModificationCommandTreeGenerator(
+                    context.GetDynamicUpdateModel(ProviderRegistry.Sql2008_ProviderInfo));
+
+            Assert.Throws<InvalidOperationException>(
+                () => new EdmModelDiffer()
+                          .Diff(
+                              model1.GetModel(),
+                              context.GetModel(),
+                              commandTreeGenerator,
+                              new SqlServerMigrationSqlGenerator())
+                          .OfType<CreateProcedureOperation>()
+                          .ToList())
+                .ValidateMessage("ErrorGeneratingCommandTree", "Thing_Insert", "Thing");
+        }
+
         [MigrationsTheory]
         public void Can_diff_identical_models_at_different_edm_versions_and_no_diffs_produced()
         {

test/EntityFramework/UnitTests/Migrations/Infrastructure/ModificationCommandTreeGeneratorTests.cs
@@ -2,12 +2,181 @@
 
 namespace System.Data.Entity.Migrations.Infrastructure
 {
+    using System.Data.Entity.Infrastructure;
     using System.Data.Entity.Migrations.Infrastructure.FunctionsModel;
+    using System.Data.Entity.Utilities;
     using System.Linq;
     using Xunit;
 
     public class ModificationCommandTreeGeneratorTests
     {
+        public class WorldContext : DbContext
+        {
+            static WorldContext()
+            {
+                Database.SetInitializer<WorldContext>(null);
+            }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder
+                    .Entity<Thing>()
+                    .MapToStoredProcedures();
+            }
+        }
+
+        public class WorldContext_Fk : WorldContext
+        {
+            static WorldContext_Fk()
+            {
+                Database.SetInitializer<WorldContext_Fk>(null);
+            }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+
+                modelBuilder
+                    .Entity<Thing>()
+                    .HasRequired(t => t.Container)
+                    .WithMany()
+                    .HasForeignKey(t => t.ContainerFk);
+            }
+        }
+
+        public class Thing
+        {
+            public int Id { get; set; }
+            public Thing Container { get; set; }
+            public int ContainerFk { get; set; }
+        }
+
+        [Fact]
+        public void Can_generate_dynamic_insert_command_trees_for_self_ref_fk()
+        {
+            DbModel model;
+
+            using (var context = new WorldContext_Fk())
+            {
+                model = context.GetDynamicUpdateModel(ProviderRegistry.Sql2008_ProviderInfo);
+            }
+
+            var commandTreeGenerator
+                = new ModificationCommandTreeGenerator(model);
+
+            var commandTrees
+                = commandTreeGenerator
+                    .GenerateInsert(GetType().Namespace + ".Thing")
+                    .ToList();
+
+            Assert.Equal(1, commandTrees.Count());
+        }
+
+        [Fact]
+        public void Can_generate_dynamic_update_command_trees_for_self_ref_fk()
+        {
+            DbModel model;
+
+            using (var context = new WorldContext_Fk())
+            {
+                model = context.GetDynamicUpdateModel(ProviderRegistry.Sql2008_ProviderInfo);
+            }
+
+            var commandTreeGenerator
+                = new ModificationCommandTreeGenerator(model);
+
+            var commandTrees
+                = commandTreeGenerator
+                    .GenerateUpdate(GetType().Namespace + ".Thing")
+                    .ToList();
+
+            Assert.Equal(1, commandTrees.Count());
+        }
+
+        [Fact]
+        public void Can_generate_dynamic_delete_command_trees_for_self_ref_fk()
+        {
+            DbModel model;
+
+            using (var context = new WorldContext_Fk())
+            {
+                model = context.GetDynamicUpdateModel(ProviderRegistry.Sql2008_ProviderInfo);
+            }
+
+            var commandTreeGenerator
+                = new ModificationCommandTreeGenerator(model);
+
+            var commandTrees
+                = commandTreeGenerator
+                    .GenerateDelete(GetType().Namespace + ".Thing")
+                    .ToList();
+
+            Assert.Equal(1, commandTrees.Count());
+        }
+
+        [Fact]
+        public void Can_generate_dynamic_insert_command_trees_for_self_ref_ia()
+        {
+            DbModel model;
+
+            using (var context = new WorldContext())
+            {
+                model = context.GetDynamicUpdateModel(ProviderRegistry.Sql2008_ProviderInfo);
+            }
+
+            var commandTreeGenerator
+                = new ModificationCommandTreeGenerator(model);
+
+            var commandTrees
+                = commandTreeGenerator
+                    .GenerateInsert(GetType().Namespace + ".Thing")
+                    .ToList();
+
+            Assert.Equal(1, commandTrees.Count());
+        }
+
+        [Fact]
+        public void Can_generate_dynamic_update_command_trees_for_self_ref_ia()
+        {
+            DbModel model;
+
+            using (var context = new WorldContext())
+            {
+                model = context.GetDynamicUpdateModel(ProviderRegistry.Sql2008_ProviderInfo);
+            }
+
+            var commandTreeGenerator
+                = new ModificationCommandTreeGenerator(model);
+
+            var commandTrees
+                = commandTreeGenerator
+                    .GenerateUpdate(GetType().Namespace + ".Thing")
+                    .ToList();
+
+            Assert.Equal(1, commandTrees.Count());
+        }
+
+        [Fact]
+        public void Can_generate_dynamic_delete_command_trees_for_self_ref_ia()
+        {
+            DbModel model;
+
+            using (var context = new WorldContext())
+            {
+                model = context.GetDynamicUpdateModel(ProviderRegistry.Sql2008_ProviderInfo);
+            }
+
+            var commandTreeGenerator
+                = new ModificationCommandTreeGenerator(model);
+
+            var commandTrees
+                = commandTreeGenerator
+                    .GenerateDelete(GetType().Namespace + ".Thing")
+                    .ToList();
+
+            Assert.Equal(1, commandTrees.Count());
+        }
+
         [Fact]
         public void Can_generate_dynamic_insert_command_trees_for_many_to_many_association()
         {

commit 4fe2a62d8a3caa77b0cf6d8ab2c1cb606a57acbf

EF 6.0.2 - Fix for #1742 - EF6 regression: identity pattern not applied to key by convention in simple inheritance scenario
Problem: Changeset #9a111e568cea (fix for #1101) introduced a known breaking change where we would no longer introduce SGP.Identity for entities with self-refs. We thought this change would be low impact but it turns out to be higher than we anticipated as it affects some common self-ref, inheritance model patterns.
Solution: Revert the change. It was originally needed to fix #1101 but an alternative solution is to ensure that we always set fake keys when performing modification command tree generation.

Work Item: 1742

src/EntityFramework/Migrations/Infrastructure/ModificationCommandTreeGenerator.cs
@@ -131,9 +131,10 @@ namespace System.Data.Entity.Migrations.Infrastructure
             var entity = InstantiateEntity(entityType, context, clrType, set);
 
             SetFakeReferenceKeyValues(entity, entityType);
+            SetFakeKeyValues(entity, entityType);
 
             set.Attach(entity);
-
+            
             return entity;
         }
 
@@ -384,7 +385,7 @@ namespace System.Data.Entity.Migrations.Infrastructure
 
                 if (value != null)
                 {
-                    clrPropertyInfo.SetValue(entity, value, null);
+                    clrPropertyInfo.GetPropertyInfoForSet().SetValue(entity, value, null);
                 }
             }
         }
@@ -421,7 +422,7 @@ namespace System.Data.Entity.Migrations.Infrastructure
 
                 Debug.Assert(value != null);
 
-                clrPropertyInfo.SetValue(entity, value, null);
+                clrPropertyInfo.GetPropertyInfoForSet().SetValue(entity, value, null);
             }
         }
 
@@ -504,7 +505,7 @@ namespace System.Data.Entity.Migrations.Infrastructure
 
                     InstantiateComplexProperties(complexObject, property.ComplexType.Properties);
 
-                    clrPropertyInfo.SetValue(structuralObject, complexObject, null);
+                    clrPropertyInfo.GetPropertyInfoForSet().SetValue(structuralObject, complexObject, null);
                 }
             }
         }

src/EntityFramework/ModelConfiguration/Conventions/Edm/StoreGeneratedIdentityKeyConvention.cs
@@ -42,27 +42,14 @@ namespace System.Data.Entity.ModelConfiguration.Conventions
                     && _applicableTypes.Contains(property.PrimitiveType.PrimitiveTypeKind))
                 {
                     if (!model.GetConceptualModel().AssociationTypes.Any(a => IsNonTableSplittingForeignKey(a, property))
-                        && !ParentOfTpc(item, model.GetConceptualModel())
-                        && !HasRequiredSelfRef(item, model.GetConceptualModel()))
+                        && !ParentOfTpc(item, model.GetConceptualModel()))
                     {
                         property.SetStoreGeneratedPattern(StoreGeneratedPattern.Identity);
                     }
                 }
             }
         }
-
-        private static bool HasRequiredSelfRef(EntityType entityType, EdmModel model)
-        {
-            DebugCheck.NotNull(entityType);
-            DebugCheck.NotNull(model);
-
-            return model.AssociationTypes
-                .Any(
-                    at => at.SourceEnd.GetEntityType().GetRootType() == entityType.GetRootType()
-                          && at.TargetEnd.GetEntityType().GetRootType() == entityType.GetRootType()
-                          && (at.SourceEnd.IsRequired() || at.TargetEnd.IsRequired()));
-        }
-
+        
         /// <summary>
         /// Checks for the PK property being an FK in a different table. A PK which is also an FK but
         /// in the same table is used for table splitting and can still be an identity column because

test/EntityFramework/FunctionalTests.Transitional/CodeFirst/InheritanceScenarioTests.cs
@@ -8,6 +8,7 @@ namespace FunctionalTests
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity;
     using System.Data.Entity.Core;
+    using System.Data.Entity.Core.Metadata.Edm;
     using System.Data.Entity.ModelConfiguration.Edm;
     using System.Linq;
     using System.Transactions;
@@ -16,6 +17,33 @@ namespace FunctionalTests
 
     public class InheritanceScenarioTests : TestBase
     {
+        public abstract class MessageBase
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Contents { get; set; }
+        }
+
+        public class Comment : MessageBase
+        {
+            public MessageBase Parent { get; set; }
+            public int ParentId { get; set; }
+        }
+
+        [Fact]
+        public void Identity_convention_applied_when_self_ref()
+        {
+            var modelBuilder = new DbModelBuilder();
+
+            modelBuilder.Entity<MessageBase>();
+            modelBuilder.Entity<Comment>();
+
+            var databaseMapping = BuildMapping(modelBuilder);
+
+            databaseMapping.AssertValid();
+            databaseMapping.Assert<MessageBase>("MessageBases").Column("Id").DbEqual(StoreGeneratedPattern.Identity, c => c.StoreGeneratedPattern);
+        }
+
         public class Person1545
         {
             public int Id { get; set; }

test/EntityFramework/UnitTests/Migrations/Infrastructure/EdmModelDifferTests.cs
@@ -139,53 +139,6 @@ namespace System.Data.Entity.Migrations.Infrastructure
             Assert.True(operations.Any(o => o is AddColumnOperation));
         }
 
-        public class WorldContext_Invalid : ModificationCommandTreeGeneratorTests.WorldContext_Fk
-        {
-            static WorldContext_Invalid()
-            {
-                Database.SetInitializer<WorldContext_Invalid>(null);
-            }
-
-            protected override void OnModelCreating(DbModelBuilder modelBuilder)
-            {
-                base.OnModelCreating(modelBuilder);
-
-                modelBuilder
-                    .Entity<ModificationCommandTreeGeneratorTests.Thing>()
-                    .Property(t => t.Id)
-                    .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
-            }
-        }
-
-        [MigrationsTheory]
-        public void Update_exceptions_should_be_wrapped_when_generating_sproc_bodies()
-        {
-            var modelBuilder = new DbModelBuilder();
-
-            var model1 = modelBuilder.Build(ProviderInfo);
-
-            var context = new WorldContext_Invalid();
-
-            var commandTreeGenerator
-                = new ModificationCommandTreeGenerator(
-                    context
-                        .InternalContext
-                        .CodeFirstModel
-                        .CachedModelBuilder
-                        .BuildDynamicUpdateModel(ProviderRegistry.Sql2008_ProviderInfo));
-
-            Assert.Throws<InvalidOperationException>(
-                () => new EdmModelDiffer()
-                    .Diff(
-                        model1.GetModel(),
-                        context.GetModel(),
-                        new Lazy<ModificationCommandTreeGenerator>(() => commandTreeGenerator),
-                        new SqlServerMigrationSqlGenerator())
-                    .OfType<CreateProcedureOperation>()
-                    .ToList())
-                .ValidateMessage("ErrorGeneratingCommandTree", "Thing_Insert", "Thing");
-        }
-
         public class TypeBase
         {
             public int Id { get; set; }

test/EntityFramework/UnitTests/Migrations/Infrastructure/ModificationCommandTreeGeneratorTests.cs
@@ -14,6 +14,152 @@ namespace System.Data.Entity.Migrations.Infrastructure
 
     public class ModificationCommandTreeGeneratorTests : TestBase
     {
+        private class WorldContext_Identity : WorldContext_Fk
+        {
+            static WorldContext_Identity()
+            {
+                Database.SetInitializer<WorldContext_Identity>(null);
+            }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+
+                modelBuilder.Entity<Thing>().Property(t => t.Id).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            }
+        }
+
+        [Fact]
+        public void Can_generate_insert_tree_when_self_ref_direct()
+        {
+            DbModel model;
+
+            using (var context = new WorldContext_Identity())
+            {
+                model = context.InternalContext.CodeFirstModel.CachedModelBuilder.BuildDynamicUpdateModel(ProviderRegistry.Sql2008_ProviderInfo);
+            }
+
+            var commandTreeGenerator = new ModificationCommandTreeGenerator(model);
+
+            var commandTrees = commandTreeGenerator.GenerateInsert(GetType().Namespace + ".Thing").ToList();
+
+            Assert.Equal(1, commandTrees.Count());
+        }
+
+        [Fact]
+        public void Can_generate_update_tree_when_self_ref_direct()
+        {
+            DbModel model;
+
+            using (var context = new WorldContext_Identity())
+            {
+                model = context.InternalContext.CodeFirstModel.CachedModelBuilder.BuildDynamicUpdateModel(ProviderRegistry.Sql2008_ProviderInfo);
+            }
+
+            var commandTreeGenerator = new ModificationCommandTreeGenerator(model);
+
+            var commandTrees = commandTreeGenerator.GenerateUpdate(GetType().Namespace + ".Thing").ToList();
+
+            Assert.Equal(1, commandTrees.Count());
+        }
+
+        [Fact]
+        public void Can_generate_delete_tree_when_self_ref_direct()
+        {
+            DbModel model;
+
+            using (var context = new WorldContext_Identity())
+            {
+                model = context.InternalContext.CodeFirstModel.CachedModelBuilder.BuildDynamicUpdateModel(ProviderRegistry.Sql2008_ProviderInfo);
+            }
+
+            var commandTreeGenerator = new ModificationCommandTreeGenerator(model);
+
+            var commandTrees = commandTreeGenerator.GenerateDelete(GetType().Namespace + ".Thing").ToList();
+
+            Assert.Equal(1, commandTrees.Count());
+        }
+
+        public abstract class MessageBase
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Contents { get; set; }
+        }
+
+        public class Message : MessageBase
+        {
+        }
+
+        public class Comment : MessageBase
+        {
+            public MessageBase Parent { get; set; }
+            public int ParentId { get; set; }
+        }
+
+        public class SelfRefInheritanceContext : DbContext
+        {
+            public DbSet<Comment> Comments { get; set; }
+            public DbSet<MessageBase> MessageBases { get; set; }
+            public DbSet<Message> Messages { get; set; }
+
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder.Entity<MessageBase>().MapToStoredProcedures();
+            }
+        }
+
+        [Fact]
+        public void Can_generate_insert_tree_when_self_ref_inheritance()
+        {
+            DbModel model;
+
+            using (var context = new SelfRefInheritanceContext())
+            {
+                model = context.InternalContext.CodeFirstModel.CachedModelBuilder.BuildDynamicUpdateModel(ProviderRegistry.Sql2008_ProviderInfo);
+            }
+
+            var commandTreeGenerator = new ModificationCommandTreeGenerator(model);
+
+            var commandTrees = commandTreeGenerator.GenerateInsert(GetType().Namespace + ".Comment").ToList();
+
+            Assert.Equal(1, commandTrees.Count());
+        }
+
+        [Fact]
+        public void Can_generate_update_tree_when_self_ref_inheritance()
+        {
+            DbModel model;
+
+            using (var context = new SelfRefInheritanceContext())
+            {
+                model = context.InternalContext.CodeFirstModel.CachedModelBuilder.BuildDynamicUpdateModel(ProviderRegistry.Sql2008_ProviderInfo);
+            }
+
+            var commandTreeGenerator = new ModificationCommandTreeGenerator(model);
+
+            var commandTrees = commandTreeGenerator.GenerateUpdate(GetType().Namespace + ".Comment").ToList();
+
+            Assert.Equal(1, commandTrees.Count());
+        }
+
+        [Fact]
+        public void Can_generate_delete_tree_when_self_ref_inheritance()
+        {
+            DbModel model;
+
+            using (var context = new SelfRefInheritanceContext())
+            {
+                model = context.InternalContext.CodeFirstModel.CachedModelBuilder.BuildDynamicUpdateModel(ProviderRegistry.Sql2008_ProviderInfo);
+            }
+
+            var commandTreeGenerator = new ModificationCommandTreeGenerator(model);
+
+            var commandTrees = commandTreeGenerator.GenerateDelete(GetType().Namespace + ".Comment").ToList();
+
+            Assert.Equal(1, commandTrees.Count());
+        }
+
         public class Landmark
         {
             public int Id { get; set; }

test/EntityFramework/UnitTests/ModelConfiguration/Conventions/Edm/StoreGeneratedIdentityKeyConventionTests.cs
@@ -154,7 +154,7 @@ namespace System.Data.Entity.ModelConfiguration.Conventions
         }
 
         [Fact]
-        public void Apply_should_not_match_required_self_ref()
+        public void Apply_should_match_required_self_ref()
         {
             var model = new EdmModel(DataSpace.CSpace);
             var entityType = new EntityType("E", "N", DataSpace.CSpace);
@@ -162,20 +162,17 @@ namespace System.Data.Entity.ModelConfiguration.Conventions
 
             entityType.AddKeyMember(property);
 
-            var associationType
-                = model.AddAssociationType(
+            model.AddAssociationType(
                     "A",
                     entityType, RelationshipMultiplicity.One,
                     entityType, RelationshipMultiplicity.Many);
 
-            model.AddAssociationType(associationType);
-
             (new StoreGeneratedIdentityKeyConvention())
                 .Apply(entityType, new DbModel(model, null));
 
-            Assert.Null(entityType.KeyProperties.Single().GetStoreGeneratedPattern());
+            Assert.NotNull(entityType.KeyProperties.Single().GetStoreGeneratedPattern());
         }
-
+        
         [Fact]
         public void Apply_should_match_optional_self_ref()
         {
@@ -185,13 +182,10 @@ namespace System.Data.Entity.ModelConfiguration.Conventions
 
             entityType.AddKeyMember(property);
 
-            var associationType
-                = model.AddAssociationType(
+            model.AddAssociationType(
                     "A",
                     entityType, RelationshipMultiplicity.ZeroOrOne,
                     entityType, RelationshipMultiplicity.Many);
-
-            model.AddAssociationType(associationType);
 
             (new StoreGeneratedIdentityKeyConvention())
                 .Apply(entityType, new DbModel(model, null));