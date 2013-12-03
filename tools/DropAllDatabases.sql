975bb41abac2349c3f95e33a15895e2193261bfa
Brice Lambson <bricelam@microsoft.com>
2013-12-03 11:08:25 -0800

    Exclude sample databases in DropAllDatabases.sql and move to tools directory

--- tools/DropAllDatabases.sql
+++ tools/DropAllDatabases.sql
@@ -1,10 +1,10 @@
--- Generates a SQL script that will drop all databases (except system ones) on the current server.
+-- Generates a SQL script that will drop all databases (except system and sample ones) on the current server.
 
 DECLARE @name nvarchar(255)
 
 DECLARE db CURSOR FOR 
 SELECT Name FROM sysdatabases
-WHERE Name NOT IN ('master', 'tempdb', 'model', 'msdb')
+WHERE Name NOT IN ('master', 'tempdb', 'model', 'msdb', 'AdventureWorks2012', 'Chinook', 'Northwind', 'pubs')
 
 OPEN db;
 
