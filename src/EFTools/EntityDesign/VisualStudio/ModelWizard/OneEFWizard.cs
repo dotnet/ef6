26fc84d84661d5bd8d04b1c873cab2473ca9cc51
Brice Lambson <bricelam@microsoft.com>
2014-01-17 17:00:47 -0800

    Add more missing documentation comments

--- src/EFTools/EntityDesign/VisualStudio/ModelWizard/OneEFWizard.cs
+++ src/EFTools/EntityDesign/VisualStudio/ModelWizard/OneEFWizard.cs
@@ -10,30 +10,38 @@ namespace Microsoft.Data.Entity.Design.VisualStudio.ModelWizard
     using Microsoft.Data.Entity.Design.VisualStudio.ModelWizard.Properties;
     using Microsoft.VisualStudio.TemplateWizard;
 
+    /// <summary>
+    /// This API supports the Entity Framework infrastructure and is not intended to be used directly from your code.
+    /// </summary>
     public class OneEFWizard : IWizard
     {
+        /// <inheritdoc />
         public void BeforeOpeningFile(ProjectItem projectItem)
         {
         }
 
+        /// <inheritdoc />
         public void ProjectFinishedGenerating(Project project)
         {
         }
 
+        /// <inheritdoc />
         public void ProjectItemFinishedGenerating(ProjectItem projectItem)
         {
         }
 
+        /// <inheritdoc />
         public void RunFinished()
         {
         }
 
+        /// <inheritdoc />
         public void RunStarted(object automationObject, Dictionary<string, string> replacementsDictionary, WizardRunKind runKind, object[] customParams)
         {
             AddReplacements(VsUtils.GetActiveProject((DTE2)automationObject), replacementsDictionary);
-
         }
 
+        /// <inheritdoc />
         public bool ShouldAddProjectItem(string filePath)
         {
             return true;
@@ -52,7 +60,7 @@ namespace Microsoft.Data.Entity.Design.VisualStudio.ModelWizard
             else
             {
                 ctorCommentTemplate = Resources.CodeFirstCodeFile_CtorComment_CS;
-                dbSetCommentTemplate = Resources.CodeFirstCodeFile_DbSetComment_CS;                
+                dbSetCommentTemplate = Resources.CodeFirstCodeFile_DbSetComment_CS;
             }
 
             // the item names used to get replacements must match names in the code file templates
