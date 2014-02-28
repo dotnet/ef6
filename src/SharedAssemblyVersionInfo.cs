2ecfd595d83551d0634174cbb72e6ef9da1658ed
Miguel Lacouture <miguell@microsoft.com>
2014-02-28 06:01:45 -0800

    Add support for SatelliteContractVersion for servicing purposes + some bug fixes.

--- src/SharedAssemblyVersionInfo.cs
+++ src/SharedAssemblyVersionInfo.cs
@@ -1,9 +1,11 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 
 using System.Reflection;
+using System.Resources;
 
 #if !BUILD_GENERATED_VERSION
 [assembly: AssemblyVersion("6.0.0.0")]
 [assembly: AssemblyFileVersion("6.1.0.0")]
 [assembly: AssemblyInformationalVersion("6.1.0-alpha1")]
+[assembly: SatelliteContractVersion("6.0.0.0")]
 #endif
