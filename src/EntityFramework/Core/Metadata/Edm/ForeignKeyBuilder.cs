5c6cae858975f25f9eb13278fa6f187bf1a7ae01
Emil Cicos <emilcico@hotmail.com>
2014-09-03 12:04:31 -0700

    CodePlex2465: Issue with navigation properties & entity splitting.
    Change 7bbd3a57f40638b7e3955e3df40fe914eff19c26 added a setter for ForeignKeyBuilder.Name. The implementation updated the name of the referenced association type but did not update the name of the referenced association set. This causes identity conflicts. The fix updates the association set name.

--- src/EntityFramework/Core/Metadata/Edm/ForeignKeyBuilder.cs
+++ src/EntityFramework/Core/Metadata/Edm/ForeignKeyBuilder.cs
@@ -40,7 +40,11 @@ namespace System.Data.Entity.Core.Metadata.Edm
         public string Name
         {
             get { return _associationType.Name; }
-            set { _associationType.Name = value; }
+            set
+            {
+                _associationType.Name = value;
+                _associationSet.Name = value;
+            }
         }
 
         public virtual EntityType PrincipalTable
