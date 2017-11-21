7e3e84775be5f4046133f1725b5903502cb75c5c
Leonid Tsarev <leo@bastilia.ru>
2017-11-21 15:44:15 +0300

    Allow query translator to translate new Guid(x.SomeStringField) to CAST(SomeStringField AS UNIQUEIDENTIFIER)

--- test/EntityFramework/FunctionalTests/Query/LinqToEntities/FunctionsTests.cs
+++ test/EntityFramework/FunctionalTests/Query/LinqToEntities/FunctionsTests.cs
@@ -338,6 +338,26 @@ namespace System.Data.Entity.Query.LinqToEntities
         }
 
         [Fact]
+        public void GuidConstructor_translated_to_correct_function_in_database()
+        {
+            using (var context = new ArubaContext())
+            {
+                var query = context.Owners.Select(o => new Guid("4b44ce33-b60e-4afd-85ad-59d3d7c53f75"));
+                Assert.Contains("CAST('4B44CE33-B60E-4AFD-85AD-59D3D7C53F75' AS UNIQUEIDENTIFIER)", query.ToString().ToUpperInvariant());
+            }
+        }
+
+        [Fact]
+        public void GuidConvert_translated_to_correct_function_in_database()
+        {
+            using (var context = new ArubaContext())
+            {
+                var query = context.AllTypes.Select(o => new Guid(o.c13_varchar_512_));
+                Assert.Contains("CAST( [EXTENT1].[C13_VARCHAR_512_] AS UNIQUEIDENTIFIER)", query.ToString().ToUpperInvariant());
+            }
+        }
+
+        [Fact]
         public void SqlFunctions_scalar_function_translated_properly_to_sql_function()
         {
             using (var context = new ArubaContext())
