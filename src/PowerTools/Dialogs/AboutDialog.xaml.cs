df4ad3a2510fee4780aa205479f7f011d1ddee42
ErikEJ <ejlskov@hotmail.com>
2017-11-29 12:29:58 +0100

    Add About dialog
    fixes #20

---
+++ src/PowerTools/Dialogs/AboutDialog.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System;
+
+namespace Microsoft.DbContextPackage
+{
+    public partial class AboutDialog
+    {
+        private readonly DbContextPackage _package;
+
+        public AboutDialog(DbContextPackage package)
+        {
+            _package = package;
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {            
+            Version.Text = "Version " + System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void CodeplexLink_Click(object sender, RoutedEventArgs e)
+        {
+            _package.DTE2.ItemOperations.Navigate("https://github.com/ErikEJ/EntityFramework6PowerTools");
+        }
+
+        private void GalleryLink_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://marketplace.visualstudio.com/items?itemName=ErikEJ.EntityFramework6PowerToolsCommunityEdition#review-details");
+        }
+    }
+}
---
+++ src/PowerTools/Handlers/AboutHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Microsoft.DbContextPackage.Handlers
+{
+    internal class AboutHandler
+    {
+        private readonly DbContextPackage _package;
+
+        public AboutHandler(DbContextPackage package)
+        {
+            _package = package;
+        }
+
+        public void ShowDialog()
+        {
+            try
+            {
+                var dialog = new AboutDialog(_package);
+                dialog.ShowDialog();
+            }
+            catch (Exception exception)
+            {
+                _package.LogError("An error occured when showing the About dialog", exception);
+            }
+        }
+    }
+}
