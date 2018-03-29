0cf7aa5e4e4442dbaebbfde23e55dfd49b77a7d7
lajones <lajones@users.noreply.github.com>
2018-03-29 14:19:50 -0700

    Fix for Accessibility Bug 586335 (#511)
    Fix for Accessibility Bug 586335 - EntityType shape, Properties and Navigation Properties compartments key handlers.

--- src/EFTools/EntityDesignEntityDesigner/CustomCode/Diagram/EntityTypeElementListCompartment.cs
+++ src/EFTools/EntityDesignEntityDesigner/CustomCode/Diagram/EntityTypeElementListCompartment.cs
@@ -114,30 +114,5 @@ namespace Microsoft.Data.Entity.Design.EntityDesigner.View
                 return Resources.AccHelp_EntityTypeNavigationPropertyCompartment;
             }
         }
-
-        internal void CollapseInTransaction()
-        {
-            if (IsExpanded)
-            {
-                using (var txn = Store.TransactionManager.BeginTransaction())
-                {
-                    Collapse();
-                    txn.Commit();
-                }
-            }
-        }
-
-
-        internal void ExpandInTransaction()
-        {
-            if (!IsExpanded)
-            {
-                using (var txn = Store.TransactionManager.BeginTransaction())
-                {
-                    Expand();
-                    txn.Commit();
-                }
-            }
-        }
     }
 }
---
+++ src/EFTools/EntityDesignEntityDesigner/CustomCode/Utils/NodeShapeExtensions.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+namespace Microsoft.Data.Entity.Design.EntityDesigner.CustomCode.Utils
+{
+    using Microsoft.VisualStudio.Modeling.Diagrams;
+
+    /// <summary>
+    ///     Helper extension method for NodeShape objects
+    /// </summary>
+    internal static class NodeShapeExtensions
+    {
+        /// <summary>
+        /// Ensures that a <see cref="NodeShape"/> has a particular expanded or collapsed state
+        /// </summary>
+        /// <param name="nodeShape">the <see cref="NodeShape"/> on which to ensure the state</param>
+        /// <param name="expanded">if true, ensure that nodeShape is expanded, if false esnure that it is collapsed</param>
+        public static void EnsureExpandedState(this NodeShape nodeShape, bool expanded)
+        {
+            if (expanded)
+            {
+                if (!nodeShape.IsExpanded)
+                {
+                    using (var txn = nodeShape.Store.TransactionManager.BeginTransaction())
+                    {
+                        nodeShape.IsExpanded = true;
+                        txn.Commit();
+                    }
+                }
+            }
+            else
+            {
+                if (nodeShape.IsExpanded)
+                {
+                    using (var txn = nodeShape.Store.TransactionManager.BeginTransaction())
+                    {
+                        nodeShape.IsExpanded = false;
+                        txn.Commit();
+                    }
+                }
+            }
+        }
+    }
+}
