00638970517b21a0443606016b16dae3924b4593
Arthur Vickers <ajcvickers@hotmail.com>
2018-08-15 10:57:34 -0700

    Updated doc comments.

--- src/EntityFramework/Core/Objects/ObjectMaterializedEventArgs.cs
+++ src/EntityFramework/Core/Objects/ObjectMaterializedEventArgs.cs
@@ -14,10 +14,10 @@ namespace System.Data.Entity.Core.Objects
         // </summary>
         private readonly object _entity;
 
-        // <summary>
-        // Constructs new arguments for the ObjectMaterialized event.
-        // </summary>
-        // <param name="entity"> The object that has been materialized. </param>
+        /// <summary>
+        /// Constructs new arguments for the ObjectMaterialized event.
+        /// </summary>
+        /// <param name="entity"> The object that has been materialized. </param>
         public ObjectMaterializedEventArgs(object entity)
         {
             _entity = entity;
