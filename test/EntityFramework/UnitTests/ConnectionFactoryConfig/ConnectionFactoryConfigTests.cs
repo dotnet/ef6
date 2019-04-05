dbbd72f4a6ed935620d39e012f8433c023c24f35
Brice Lambson <brice@bricelam.net>
2019-04-05 13:54:14 -0700

    Only test on net452

--- test/EntityFramework/UnitTests/ConnectionFactoryConfig/ConnectionFactoryConfigTests.cs
+++ test/EntityFramework/UnitTests/ConnectionFactoryConfig/ConnectionFactoryConfigTests.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 
-#if NET45
+#if NET452
 
 namespace System.Data.Entity.ConnectionFactoryConfig
 {
