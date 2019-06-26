248330348ea7b5d756fa4bab52d5cebc98887fde
Connor Pearson <cjp822@gmail.com>
2019-06-26 13:59:31 -0400

    Query compilation performance (#430)
    * lower MaxNodeCountForTransformations
    * use bitwise operations for VarVec.MoveNext and VarVec.Subsumes
    * Store reverseMap instead of calculating it each time

--- src/EntityFramework/Core/Query/PlanCompiler/PlanCompiler.cs
+++ src/EntityFramework/Core/Query/PlanCompiler/PlanCompiler.cs
@@ -57,7 +57,7 @@ namespace System.Data.Entity.Core.Query.PlanCompiler
         // Determines the maximum size of the query in terms of Iqt nodes for which we attempt to do transformation rules.
         // This number is ignored if applyTransformationsRegardlessOfSize is enabled.
         // </summary>
-        private const int MaxNodeCountForTransformations = 100000;
+        private const int MaxNodeCountForTransformations = 10000;
 
         // <summary>
         // The CTree we're compiling a plan for.
