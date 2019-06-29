2db68c409cc170b1772fda46b35d18b7ebf966c3
Nick Schonning <nschonni@gmail.com>
2019-06-29 04:07:44 -0400

    fix: T:System.decimal -> T:System.Decimal

--- src/EntityFramework/ModelConfiguration/Configuration/Types/StructuralTypeConfiguration`.cs
+++ src/EntityFramework/ModelConfiguration/Configuration/Types/StructuralTypeConfiguration`.cs
@@ -116,7 +116,7 @@ namespace System.Data.Entity.ModelConfiguration.Configuration
         }
 
         /// <summary>
-        /// Configures a <see cref="T:System.decimal" /> property that is defined on this type.
+        /// Configures a <see cref="T:System.Decimal" /> property that is defined on this type.
         /// </summary>
         /// <param name="propertyExpression"> A lambda expression representing the property to be configured. C#: t => t.MyProperty VB.Net: Function(t) t.MyProperty </param>
         /// <returns> A configuration object that can be used to configure the property. </returns>
@@ -129,7 +129,7 @@ namespace System.Data.Entity.ModelConfiguration.Configuration
         }
 
         /// <summary>
-        /// Configures a <see cref="T:System.decimal" /> property that is defined on this type.
+        /// Configures a <see cref="T:System.Decimal" /> property that is defined on this type.
         /// </summary>
         /// <param name="propertyExpression"> A lambda expression representing the property to be configured. C#: t => t.MyProperty VB.Net: Function(t) t.MyProperty </param>
         /// <returns> A configuration object that can be used to configure the property. </returns>
