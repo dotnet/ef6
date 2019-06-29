60c7d290d87576508a78f4d3f02409d0f3a8de89
Nick Schonning <nschonni@gmail.com>
2019-06-29 18:44:45 -0400

    typo: implicity -> implicitly

--- src/EntityFramework/Core/Query/ResultAssembly/BridgeDataRecord.cs
+++ src/EntityFramework/Core/Query/ResultAssembly/BridgeDataRecord.cs
@@ -345,7 +345,7 @@ namespace System.Data.Entity.Core.Query.ResultAssembly
         }
 
         // <summary>
-        // True so long as we haven't been closed either implicity or explicitly
+        // True so long as we haven't been closed either implicitly or explicitly
         // </summary>
         internal bool IsClosed
         {
