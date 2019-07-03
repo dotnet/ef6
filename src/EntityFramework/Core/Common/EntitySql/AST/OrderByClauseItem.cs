e8e874ca85f3ff6d9dbc894d7a49de45e02fcefa
Nick Schonning <nschonni@gmail.com>
2019-07-03 19:09:45 -0400

    typo: Oeturns -> Returns

--- src/EntityFramework/Core/Common/EntitySql/AST/OrderByClauseItem.cs
+++ src/EntityFramework/Core/Common/EntitySql/AST/OrderByClauseItem.cs
@@ -33,7 +33,7 @@ namespace System.Data.Entity.Core.Common.EntitySql.AST
         }
 
         // <summary>
-        // Oeturns order expression.
+        // Returns order expression.
         // </summary>
         internal Node OrderExpr
         {
