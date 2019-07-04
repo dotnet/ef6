36b2b7078fb8d4b2addb7599b7477bdcbfa4656b
Nick Schonning <nschonni@gmail.com>
2019-07-04 01:27:57 -0400

    typo: assoication -> association

--- src/EntityFramework/Core/Metadata/Edm/EntityType.cs
+++ src/EntityFramework/Core/Metadata/Edm/EntityType.cs
@@ -255,7 +255,7 @@ namespace System.Data.Entity.Core.Metadata.Edm
         }
 
         // <summary>
-        // Attempts to get the property name for the assoication between the two given end
+        // Attempts to get the property name for the association between the two given end
         // names.  Note that this property may not exist if a navigation property is defined
         // in one direction but not in the other.
         // </summary>
