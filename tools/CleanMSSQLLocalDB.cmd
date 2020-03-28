23a7d04b908fd196408f32ffd8c2e4d99aaa1b4a
Doug Bunting <6431421+dougbu@users.noreply.github.com>
2020-03-27 18:57:51 -0700

    Update tools/CleanMSSQLLocalDB.cmd
    - ensure MSSQLLocalDb instance is started

--- tools/CleanMSSQLLocalDB.cmd
+++ tools/CleanMSSQLLocalDB.cmd
@@ -1,5 +1,7 @@
 @echo off
-sqlcmd -S "(localdb)\mssqllocaldb" -i "%~dp0DropAllDatabases.sql" -o "DropAll.sql"
+SqlLocalDB start
+sqlcmd -S "(localdb)\mssqllocaldb" -i "%~dp0DropAllDatabases.sql" -l 32 -r0 >"DropAll.sql"
+echo Will execute:
 type DropAll.sql
 sqlcmd -S "(localdb)\mssqllocaldb" -i "DropAll.sql"
 del "DropAll.sql"
