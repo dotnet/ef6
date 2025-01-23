cb8ee7a70568636a304423ed5520415117da018f
ashtilawat23 <ashtilawat23@gmail.com>
2025-01-23 09:59:55 -0500

    demo c# code

---
+++ demo/Program.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from the demo!");
+            
+            // Create and use a test class
+            var test = new TestClass();
+            test.RunTest();
+        }
+    }
+} 
---
+++ demo/TestClass.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Demo
+{
+    public class TestClass
+    {
+        public void RunTest()
+        {
+            Console.WriteLine("Running test...");
+            // Add your test logic here
+            Console.WriteLine("Test completed.");
+        }
+    }
+} 
